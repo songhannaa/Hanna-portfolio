9365ead341979fdb9c4b7f2d99febb13bbce4853
songhannaa <shn0322@kakao.com>
2024-08-20 02:05:23 +0900

    hanna portfolio pic

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,65 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+// 모달 변수 선언
+var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
+var modal3 = document.getElementById("myModal3");
+
+// 버튼 변수 선언
+var btn = document.getElementById("openModal");
+var btn2 = document.getElementById("openModal2");
+var btn3 = document.getElementById("openModal3");
+
+// 닫기 버튼 변수 선언
+var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close2")[0];
+var span3 = document.getElementsByClassName("close3")[0];
+
+// 첫 번째 모달 열기 및 닫기
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+// 두 번째 모달 열기 및 닫기
+btn2.onclick = function() {
+    modal2.style.display = "block";
+}
+
+span2.onclick = function() {
+    modal2.style.display = "none";
+}
+
+
+
+// 세 번째 모달 열기 및 닫기
+btn3.onclick = function() {
+    modal3.style.display = "block";
+}
+
+span3.onclick = function() {
+    modal3.style.display = "none";
+}
+
+window.onclick = function(event) {
+    // 첫 번째 모달 외부 클릭 시 닫기
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    
+    // 두 번째 모달 외부 클릭 시 닫기
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+    
+    // 세 번째 모달 외부 클릭 시 닫기
+    if (event.target == modal3) {
+        modal3.style.display = "none";
+    }
+}
